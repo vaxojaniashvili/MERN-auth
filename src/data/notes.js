dbae43cd2a2a1ef7fcf4f535f352c679bcd4d4f5
vaxo <vaxojaniashvili186@gmail.com>
2024-08-10 14:15:58 +0400

    adad

---
+++ src/data/notes.js
@@ -0,0 +1,22 @@
+export const notes = [
+    {
+      _id: "1",
+      title: "dsd",
+      content:
+        "dsdsd",
+      category: "dsdsds",
+    },
+    {
+      _id: "2",
+      title: "dsd",
+      content: "ds",
+      category: "Learning",
+    },
+    {
+      _id: "3",
+      title: "dsds",
+      content: "dsd",
+      category: "dsds",
+    },
+  ];
+  
---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const userAuth = async function (req, res, next) {
+  try {
+    const authHeader = req.headers.authorization;
+    if (authHeader === undefined) {
+      return res.status(401).send({
+        status: false,
+        message: "User is Unauthorized",
+      });
+    }
+
+    const token = req.headers.authorization.split(" ")[1];
+
+    const verifyToken = jwt.verify(
+      token,
+      process.env.JWT_SECRET,
+      (err, decode) => {
+        if (err) {
+          if (err.name === "JsonWebTokenError") {
+            return res.status(401).send({ msg: "User is Unauthorized" });
+          } else {
+            return res.status(401).send({ msg: err.message });
+          }
+        } else {
+          req.user = decode.id;
+          next();
+        }
+      }
+    );
+  } catch (err) {
+    console.log("This is the error :", err.message);
+    res.status(500).send({ msg: "Error", error: err.message });
+  }
+};
